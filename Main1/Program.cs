d4188f06a51817e3ec3ffa00505e2388d89595dd
rr1980 <rener1980@googlemail.com>
2018-05-04 15:25:46 +0200

    Projektdateien hinzufügen.

---
+++ Main1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Main1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
